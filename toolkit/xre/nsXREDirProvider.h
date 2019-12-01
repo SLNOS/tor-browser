04e6899d28554656b18be5e02bfe58d4e430161b
SLNOS <anonymous@wired>
2019-12-01 00:00:00 +0000

    Revert "Bug 9173: Change the default Firefox profile directory to be TBB-relative."
    This reverts commit 84af2ff8f89d5b9e0f5ee032d1229eb344e08dab.

--- toolkit/xre/nsXREDirProvider.h
+++ toolkit/xre/nsXREDirProvider.h
@@ -62,19 +62,15 @@ class nsXREDirProvider final : public nsIDirectoryServiceProvider2,
 
   void DoShutdown();
 
-  nsresult GetUserAppDataDirectory(nsIFile** aFile) {
+  static nsresult GetUserAppDataDirectory(nsIFile** aFile) {
     return GetUserDataDirectory(aFile, false);
   }
-  nsresult GetUserLocalDataDirectory(nsIFile** aFile) {
+  static nsresult GetUserLocalDataDirectory(nsIFile** aFile) {
     return GetUserDataDirectory(aFile, true);
   }
 
   // GetUserDataDirectory gets the profile path from gAppData.
-
-  // This function now calls GetAppDir(), so it cannot be static anymore.
-  // The same happens with all the functions (in)directly calling this one (the
-  // rest of Get*Directory functions in this file)
-  nsresult GetUserDataDirectory(nsIFile** aFile, bool aLocal);
+  static nsresult GetUserDataDirectory(nsIFile** aFile, bool aLocal);
 
   /* make sure you clone it, if you need to do stuff to it */
   nsIFile* GetGREDir() { return mGREDir; }
@@ -115,9 +111,9 @@ class nsXREDirProvider final : public nsIDirectoryServiceProvider2,
  protected:
   nsresult GetFilesInternal(const char* aProperty,
                             nsISimpleEnumerator** aResult);
-  nsresult GetUserDataDirectoryHome(nsIFile** aFile, bool aLocal);
-  nsresult GetSysUserExtensionsDirectory(nsIFile** aFile);
-  nsresult GetSysUserExtensionsDevDirectory(nsIFile** aFile);
+  static nsresult GetUserDataDirectoryHome(nsIFile** aFile, bool aLocal);
+  static nsresult GetSysUserExtensionsDirectory(nsIFile** aFile);
+  static nsresult GetSysUserExtensionsDevDirectory(nsIFile** aFile);
 #if defined(XP_UNIX) || defined(XP_MACOSX)
   static nsresult GetSystemExtensionsDirectory(nsIFile** aFile);
 #endif
